ae4d8f74fddbb394ea28e9a20ada8ea63920c973
Pedro Bastos <modappmania@gmail.com>
2026-09-10 23:04:52 -0300

    Permitir Flask SocketIO no servidor Oracle

--- main.py
+++ main.py
@@ -1797,4 +1797,9 @@ if __name__ == "__main__":
     cron_invasao_thread.start()
     
     print("🚀 Servidor Web de Eldora rodando em http://localhost:5000")
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=5000,
+        allow_unsafe_werkzeug=True
+    )
